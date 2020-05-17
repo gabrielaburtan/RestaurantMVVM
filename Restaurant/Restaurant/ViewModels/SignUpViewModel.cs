0723f31f77c1bef9aae24dfc5dc1582410cbd956
Gabriela Burtan <burtangabriela@gmail.com>
2020-05-17 19:18:22 +0300

    Added ViewModels
    Added ViewModels for SignInView, SignUpView, StartWindow.

---
+++ Restaurant/Restaurant/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Restaurant.Services;
+using Restaurant.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Restaurant.ViewModels
+{
+    class SignUpViewModel:BaseViewModel
+    {
+        private ICommand backCommand;
+        public ICommand BackCommand
+        {
+            get
+            {
+                if (backCommand == null)
+                {
+                    backCommand = new RelayCommand(BackMethod);
+                }
+                return backCommand;
+            }
+        }
+        private void BackMethod(object param)
+        {
+            StartWindow startWindow = new StartWindow();
+            App.Current.MainWindow.Close();
+            App.Current.MainWindow = startWindow;
+            startWindow.Show();
+        }
+    }
+}
---
+++ Restaurant/Restaurant/ViewModels/StartWindowViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Restaurant.Services;
+using Restaurant.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Restaurant.ViewModels
+{
+    class StartWindowViewModel : BaseViewModel
+    {
+        private ICommand signInCommand;
+        public ICommand SignInCommand
+        {
+            get
+            {
+                if (signInCommand == null)
+                {
+                    signInCommand = new RelayCommand(SignInMethod);
+                }
+                return signInCommand;
+            }
+        }
+        private void SignInMethod(object param)
+        {
+            SignInView signInView = new SignInView();
+            App.Current.MainWindow.Close();
+            App.Current.MainWindow = signInView;
+            signInView.Show();
+        }
+
+        private ICommand signUpCommand;
+        public ICommand SignUpCommand
+        {
+            get
+            {
+                if (signUpCommand == null)
+                {
+                    signUpCommand = new RelayCommand(SignUpMethod);
+                }
+                return signUpCommand;
+            }
+        }
+        private void SignUpMethod(object param)
+        {
+            SignUpView signUpView = new SignUpView();
+            App.Current.MainWindow.Close();
+            App.Current.MainWindow = signUpView;
+            signUpView.Show();
+        }
+    }
+}
